2b03a95d0189b41cbe5dba36a7b52676104e08b5
two_e <evgeniy_e@maximail.org>
2024-12-01 21:27:32 +0500

    created profile sample

--- src/pages/profile/profile.tmpl.ts
+++ src/pages/profile/profile.tmpl.ts
@@ -1,70 +1,77 @@
 // language=hbs
 export const profileTmpl = `
     <main class="profile">
-        <div class="container">
-            <div class="heading">
-                <img class="avatar" alt="avatar" src="#">
-                <h3 class="name-title">{{ user.name }}</h3>
-            </div>
-
-            <form class="info-form">
-                <div class="field">
-                    <label class="label label_email" for="email">Почта</label>
-                    <input class="input input_email disable" name="email" disabled value="{{ user.email }}">
-                </div>
-
-                <div class="field">
-                    <label class="label label_login" for="login">Логин</label>
-                    <input class="input input_login disable" name="login" disabled value="{{ user.login }}">
-                </div>
-
-                <div class="field">
-                    <label class="label label_first-name" for="first_name">Имя</label>
-                    <input class="input input_first-name disable" name="first_name" disabled value="{{ user.first_name }}">
-                </div>
-
-                <div class="field">
-                    <label class="label label_second_name" for="second_name">Фамилия</label>
-                    <input class="input input_second_name disable" name="second_name" disabled value="{{ user.second_name }}">
-                </div>
-
-                <div class="field">
-                    <label class="label label_nickname" for="nickname">Никнейм</label>
-                    <input class="input input_nickname disable" name="nickname" disabled value="{{ user.nickname }}">
-                </div>
-
-                <div class="field">
-                    <label class="label label_phone" for="phone">Телефон</label>
-                    <input class="input input_phone disable" name="phone" type="number" disabled value="{{ user.phone }}">
-                </div>
-
-                <button class="submit hidden">{{ buttonText }}</button>
-            </form>
-            
-            <div class="controls">
-                <button class="edit-info">Изменить данные</button>
-                <button class="edit-password">Изменить пароль</button>
-                <button class="logout">Выйти</button>
-            </div>
-            
-            <form class="password-form hidden">
-                <div class="field">
-                    <label class="label label_old-password" for="old_password">Старый пароль</label>
-                    <input class="input input_old-password" name="old_password" type="password">
-                </div>
-
-                <div class="field">
-                    <label class="label label_password" for="password">Новый пароль</label>
-                    <input class="input input_password" name="password" type="password">
-                </div>
-
-                <div class="field">
-                    <label class="label label_new-password" for="new_password">Новый пароль (ещё раз)</label>
-                    <input class="input input_new-password" name="new_password" type="password">
-                </div>
-
-                <button class="submit">{{ buttonText }}</button>
-            </form>
+        <aside class="aside">
+            <a class="back" href="/"></a>
+        </aside>
+        <div class="content">
+          <div class="container">
+              <div class="heading">
+                  <img class="avatar" alt="avatar" src="#">
+                  <h3 class="name-title">{{ user.name }}</h3>
+              </div>
+  
+              <form class="info-form">
+                  <div class="grid-container">
+                    <div class="field">
+                        <label class="label label_email" for="email">Электронная почта</label>
+                        <input class="input input_email disable" name="email" disabled value="{{ user.email }}">
+                    </div>
+    
+                    <div class="field">
+                        <label class="label label_login" for="login">Логин</label>
+                        <input class="input input_login disable" name="login" disabled value="{{ user.login }}">
+                    </div>
+    
+                    <div class="field">
+                        <label class="label label_first-name" for="first_name">Имя</label>
+                        <input class="input input_first-name disable" name="first_name" disabled value="{{ user.first_name }}">
+                    </div>
+    
+                    <div class="field">
+                        <label class="label label_second_name" for="second_name">Фамилия</label>
+                        <input class="input input_second_name disable" name="second_name" disabled value="{{ user.second_name }}">
+                    </div>
+    
+                    <div class="field">
+                        <label class="label label_nickname" for="nickname">Никнейм</label>
+                        <input class="input input_nickname disable" name="nickname" disabled value="{{ user.nickname }}">
+                    </div>
+    
+                    <div class="field">
+                        <label class="label label_phone" for="phone">Номер телефона</label>
+                        <input class="input input_phone disable" name="phone" type="number" disabled value="{{ user.phone }}">
+                    </div>
+                  </div>
+  
+                  <button class="submit hidden">{{ buttonText }}</button>
+              </form>
+              
+              <div class="controls">
+                  <button class="button edit-info">Изменить данные</button>
+                  <button class="button edit-password">Изменить пароль</button>
+                  <button class="button logout">Выйти</button>
+              </div>
+              
+              <form class="password-form hidden">
+                  <div class="field">
+                      <label class="label label_old-password" for="old_password">Старый пароль</label>
+                      <input class="input input_old-password" name="old_password" type="password">
+                  </div>
+  
+                  <div class="field">
+                      <label class="label label_password" for="password">Новый пароль</label>
+                      <input class="input input_password" name="password" type="password">
+                  </div>
+  
+                  <div class="field">
+                      <label class="label label_new-password" for="new_password">Новый пароль (ещё раз)</label>
+                      <input class="input input_new-password" name="new_password" type="password">
+                  </div>
+  
+                  <button class="submit">{{ buttonText }}</button>
+              </form>
+          </div>
         </div>
     </main>
 `
